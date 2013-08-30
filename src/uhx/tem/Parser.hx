69c656e33c5c762310b5a12eae84c51b2dabe35f
Skial Bainn <skial.bainn@gmail.com>
2013-08-30 11:50:32 +0100

    Minor. Further code reuse can be easily achieved, but I'm coding as I go

--- src/uhx/tem/Parser.hx
+++ src/uhx/tem/Parser.hx
@@ -149,11 +149,12 @@ class Parser {
 		var nfields = ['set_single_$domName', 'set_$domName'].mkFields().mkPublic().toFFun();
 		var single = nfields.get( 'set_single_$domName' );
 		var mass = nfields.get( 'set_$domName' );
+		var isIterable = ctype.toType().isIterable();
 		var expr = attribute 
 			? macro dtx.single.ElementManipulation.setAttr( children.getNode( pos ), $v { attName }, Std.string( v ) )
 			: macro dtx.single.ElementManipulation.setText( children.getNode( pos ), Std.string( v ) );
 		
-		switch ( [attribute, ctype.toType().isIterable()] ) {
+		switch ( [attribute, isIterable] ) {
 			case [false, false]:
 				single.body( macro dtx.single.ElementManipulation.setText($i { domName }, '' + v) );
 				
@@ -184,7 +185,8 @@ class Parser {
 				} );
 				
 		}
-		if (ctype.toType().isIterable()) {
+		
+		if (isIterable) {
 			mass.args().push( 'v'.mkArg( ctype ) );
 			
 			single.args().push( 'pos'.mkArg( macro: Int ) );
@@ -197,124 +199,6 @@ class Parser {
 		for (nf in nfields) {
 			fields.push( nf );
 		}
-		
-		/*switch ( [attribute, type.isIterable()] ) {
-						case [true, false]:
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro dtx.single.ElementManipulation.setAttr($i { domName }, $v { attName }, '' + v) )
-							);
-							fields.get( id ).args().push( 'v'.mkArg( t, false ) );
-							
-						case [false, false]:
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro dtx.single.ElementManipulation.setText($i { domName }, '' + v) )
-							);
-							fields.get( id ).args().push( 'v'.mkArg( t, false ) );
-							
-						/*case [_, true]:
-							var id = 'set_INDIV${prefix}_$name';
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro {
-									var child = $i { 'get_${prefix}_$name' } .children()[ pos ];
-									if (child == null ) {
-										child = dtx.Tools.parse('<div></div>');
-										// Need to insert a new DOM element, matching the first child's node type.
-										// Probably better to create a help class to handle all of this instead of
-										// relying on macro generation.
-									}
-									dtx.single.ElementManipulation.setAttr( child, $v { name }, '' );
-								} )
-							);
-							fields.get( id ).args().push( 'pos'.mkArg( macro: Int, false ) );*/
-							
-						/*case [true, true]:
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro {
-									var dom = dtx.single.Traversing.children( $i { 'get_$domName' }(), true );
-									for (i in 0...v.length) {
-										//var _v = v[i];
-										if (dom.collection.length > i) {
-											dtx.single.ElementManipulation.setAttr( dom.getNode( i ), $v{ attName }, '' + v[i] );
-										} else {
-											dom.add( dtx.single.ElementManipulation.setAttr( dtx.Tools.create( 'div' ), $v{ attName }, '' + v[i] ), i );
-										}
-									}
-								} )
-							);
-							fields.get( id ).args().push( 'v'.mkArg( t, false ) );
-							
-							id = 'set_single_$domName';
-							
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro {
-									var dom = dtx.single.Traversing.children( $i { 'get_$domName' } (), true );
-									if (dom.collection.length > pos) {
-										dtx.single.ElementManipulation.setAttr( dom.getNode( pos ), $v { attName }, '' + v );
-									} else {
-										var n = dtx.Tools.create( dtx.single.ElementManipulation.tagName( dom.getNode() ) );
-										dtx.single.ElementManipulation.setAttr( n, $v { attName }, '' + v );
-										dtx.single.DOMManipulation.append( $i{'get_$domName'}(), n );
-									}
-								} )
-							);
-							fields.get( id ).args().push( 'pos'.mkArg( macro: Int, false ) );
-							fields.get( id ).args().push( 'v'.mkArg( t, false ) );
-							
-						case [false, true]:
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro {
-									var dom = dtx.single.Traversing.children( $i { 'get_$domName' }(), true );
-									for (i in 0...v.length) {
-										//var _v = v[i];
-										if (dom.collection.length > i) {
-											dtx.single.ElementManipulation.setText( dom.getNode( i ), '' + v[i] );
-										} else {
-											dom.add( dtx.single.ElementManipulation.setText( dtx.Tools.create( 'div' ), '' + v[i] ), i );
-										}
-									}
-								} )
-							);
-							fields.get( id ).args().push( 'v'.mkArg( t, false ) );
-							
-							id = 'set_single_$domName';
-							
-							fields.push( id.mkField()
-								.mkPublic()
-								.toFFun()
-								.body( macro {
-									var dom:dtx.DOMNode = $i { 'get_$domName' } ();
-									var children:dtx.DOMCollection = dtx.single.Traversing.children( dom, true );
-									if (pos > (children.collection.length-1)) {
-										
-										var collection:dtx.DOMCollection = new dtx.DOMCollection();
-										for (i in 0...(pos - (children.collection.length-1))) {
-											collection.add( dtx.Tools.create( dtx.single.ElementManipulation.tagName( children.getNode() ) ) );
-										}
-										dom.append( null, collection );
-										children = dtx.single.Traversing.children( dom, true );
-										// TODO create the correct amount of elements and then set the element at `pos` with value of `v`
-										dtx.single.ElementManipulation.setText( children.getNode( pos ), '' + v );
-									} else {
-										dtx.single.ElementManipulation.setText( children.getNode( pos ), '' + v );
-									}
-								} )
-							);
-							fields.get( id ).args().push( 'pos'.mkArg( macro: Int, false ) );
-							fields.get( id ).args().push( 'v'.mkArg( t, false ) );
-							
-					}*/
 	}
 	
 	private static function mkParseField(name:String, ctype:ComplexType):Field {
