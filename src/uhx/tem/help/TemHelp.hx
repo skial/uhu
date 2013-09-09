4cb3c8696473e4f1c7eda23d0b653cb43f807902
Skial Bainn <skial.bainn@gmail.com>
2013-09-09 14:25:21 +0100

    Still recovering from idiot mode.

--- src/uhx/tem/help/TemHelp.hx
+++ src/uhx/tem/help/TemHelp.hx
@@ -1,4 +1,7 @@
 package uhx.tem.help;
+import dtx.DOMNode;
+
+using Detox;
 
 /**
  * ...
@@ -7,61 +10,74 @@ package uhx.tem.help;
 
 class TemHelp {
 	
-	@:noCompletion public static function parseSingle<T>(name:String, ele:dtx.DOMNode, attr:Bool, parse:String->dtx.DOMNode->T):T { 
-		var v:String = attr ? dtx.single.ElementManipulation.attr( ele, name ) : dtx.single.ElementManipulation.text( ele );
+	public static function parseSingle<T>(name:String, ele:DOMNode, attr:Bool, parse:String->dtx.DOMNode->T):T { 
+		//var v:String = attr ? dtx.single.ElementManipulation.attr( ele, name ) : dtx.single.ElementManipulation.text( ele );
+		var v:String = attr ? ele.attr( name ) : ele.text();
 		return parse( v, ele );
 	}
 	
-	@:noCompletion public static function parseCollection<T>(name:String, ele:dtx.DOMNode, attr:Bool, parse:String->dtx.DOMNode->T):Array<T> {  
+	public static function parseCollection<T>(name:String, ele:DOMNode, attr:Bool, parse:String->dtx.DOMNode->T):Array<T> {  
 		var r:Array<T> = [];
 		var v:String = '';
 		for (child in dtx.single.Traversing.children( ele, true )) {
-			v = attr ? dtx.single.ElementManipulation.attr( child, name ) : dtx.single.ElementManipulation.text( child );
+			//v = attr ? dtx.single.ElementManipulation.attr( child, name ) : dtx.single.ElementManipulation.text( child );
+			v = attr ? child.attr( name ) : child.text();
 			r.push( parse( v, child ) );
 		}
 		return r;
 	}
 	
-	@:noCompletion public static function setIndividual<T>(value:T, dom:dtx.DOMNode, ?attr:String) { 
-		attr == null 
+	public static function setIndividual<T>(value:T, dom:DOMNode, ?attr:String) { 
+		/*attr == null 
 			? dtx.single.ElementManipulation.setText(dom, Std.string( value ))
-			: dtx.single.ElementManipulation.setAttr(dom, attr, Std.string( value ));
+			: dtx.single.ElementManipulation.setAttr(dom, attr, Std.string( value ));*/
+		attr == null ? dom.setText( Std.string( value ) ) : dom.setAttr( attr, Std.string( value ) );
 	}
 	
-	@:noCompletion public static function setCollection<T>(value:Array<T>, dom:dtx.DOMNode, ?attr:String) {
+	public static function setCollection<T>(value:Array<T>, dom:DOMNode, ?attr:String) {
 		for (i in 0...value.length) {
 			setCollectionIndividual( value[i], i, dom, attr );
 		}
 	}
 	
-	@:noCompletion public static function setCollectionIndividual<T>(value:T, pos:Int, dom:dtx.DOMNode, ?attr:String) { 
-		var children = dtx.single.Traversing.children( dom );
+	public static function setCollectionIndividual<T>(value:T, pos:Int, dom:DOMNode, ?attr:String) { 
+		//var children = dtx.single.Traversing.children( dom );
+		var children = dom.children();
 		if (children.collection.length > pos) {
-			attr != null
+			/*attr != null
 				? dtx.single.ElementManipulation.setAttr( children.getNode( pos ), attr, Std.string( value ) )
-				: dtx.single.ElementManipulation.setText( children.getNode( pos ), Std.string( value ) );
+				: dtx.single.ElementManipulation.setText( children.getNode( pos ), Std.string( value ) );*/
+			attr != null 
+				? children.getNode( pos ).setAttr( attr, Std.string( value ) )
+				: children.getNode( pos ).setText( Std.string( value ) );
 		} else {
 			var c = new dtx.DOMCollection();
 			for (i in 0...(pos-(children.collection.length-1))) {
-				c.add( dtx.Tools.create( dtx.single.ElementManipulation.tagName( children.getNode() ) ) );
+				//c.add( dtx.Tools.create( dtx.single.ElementManipulation.tagName( children.getNode() ) ) );
+				c.add( children.getNode().tagName().create() );
 			}
-			dtx.single.DOMManipulation.append( dom, null, c );
-			children = dtx.single.Traversing.children( dom );
-			attr != null 
+			//dtx.single.DOMManipulation.append( dom, null, c );
+			dom.append( null, c );
+			//children = dtx.single.Traversing.children( dom );
+			children = dom.children();
+			/*attr != null
 				? dtx.single.ElementManipulation.setAttr( children.getNode( pos ), attr, Std.string( value ) )
-				: dtx.single.ElementManipulation.setText( children.getNode( pos ), Std.string( value ) );
+				: dtx.single.ElementManipulation.setText( children.getNode( pos ), Std.string( value ) );*/
+			attr != null 
+				? children.getNode( pos ).setAttr( attr, Std.string( value ) )
+				: children.getNode( pos ).setText( Std.string( value ) );
 		}
 	}
 	
-	public static var parserMap:Map<String, String->dtx.DOMNode->Dynamic> = [
+	public static var parserMap:Map<String, String->DOMNode->Dynamic> = [
 	'String' => parseString,
 	'Int' => parseInt,
 	'Float' => parseFloat,
 	'Bool' => parseBool,
-	
+	'Node' => parseNode,
 	];
 	
-	public static function find(type:String):String->dtx.DOMNode->Dynamic {
+	public static function find(type:String):String->DOMNode->Dynamic {
 		return parserMap.get( type );
 	}
 	
@@ -69,5 +85,6 @@ class TemHelp {
 	public static function parseFloat(value:String, _):Float return Std.parseFloat( value );
 	public static function parseInt(value:String, _):Int return Std.parseInt( value );
 	public static function parseBool(value:String, _):Bool return value == 'true' ? true : false;
+	public static function parseNode(value:String, node:DOMNode) return { };
 	
 }
