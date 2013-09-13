d6c544e5d70f3697712d42ad6c0278a91b899445
Skial Bainn <skial.bainn@gmail.com>
2013-09-13 10:17:45 +0100

    minor

--- src/uhx/tem/help/TemHelp.hx
+++ src/uhx/tem/help/TemHelp.hx
@@ -10,29 +10,11 @@ using Detox;
 
 class TemHelp {
 	
-	/*public static function parseSingle<T>(name:String, ele:DOMNode, attr:Bool, parse:String->dtx.DOMNode->T):T { 
-		var v:String = attr ? ele.attr( name ) : ele.text();
-		return parse( v, ele );
-	}
-	
-	public static function parseCollection<T>(name:String, ele:DOMNode, attr:Bool, parse:String->dtx.DOMNode->T):Array<T> {  
-		var r:Array<T> = [];
-		var v:String = '';
-		for (child in dtx.single.Traversing.children( ele, true )) {
-			v = attr ? child.attr( name ) : child.text();
-			r.push( parse( v, child ) );
-		}
-		return r;
-	}*/
-	
 	public static function setIndividual<T>(value:T, dom:DOMNode, setter:Dynamic->DOMNode->Void, ?attr:String) { 
 		attr == null ? /*dom.setText( Std.string( value ) )*/ setter( value, dom ) : dom.setAttr( attr, Std.string( value ) );
 	}
 	
 	public static function setCollection<T>(values:Iterable<T>, dom:DOMNode, setter:Dynamic->DOMNode->Void, ?attr:String) {
-		/*for (i in 0...value.length) {
-			setCollectionIndividual( value[i], i, dom, setter, attr );
-		}*/
 		var pos = 0;
 		for (value in values) {
 			setCollectionIndividual( value, pos, dom, setter, attr );
@@ -45,7 +27,6 @@ class TemHelp {
 		if (children.collection.length > pos) {
 			attr != null 
 				? children.getNode( pos ).setAttr( attr, Std.string( value ) )
-				//: children.getNode( pos ).setText( Std.string( value ) );
 				: setter( value, children.getNode( pos ) );
 		} else {
 			var c = new dtx.DOMCollection();
@@ -56,7 +37,6 @@ class TemHelp {
 			children = dom.children();
 			attr != null 
 				? children.getNode( pos ).setAttr( attr, Std.string( value ) )
-				//: children.getNode( pos ).setText( Std.string( value ) );
 				: setter( value, children.getNode( pos ) );
 		}
 	}
@@ -89,18 +69,6 @@ class TemHelp {
 	'DOMCollection' => parseDOMCollection,
 	
 	];
-	
-	/*public static function parseString(value:String, _):String return value;
-	public static function parseFloat(value:String, _):Float return Std.parseFloat( value );
-	public static function parseInt(value:String, _):Int return Std.parseInt( value );
-	public static function parseBool(value:String, _):Bool return value == 'true' ? true : false;
-	public static function parseXml(value:String, ele:DOMNode):Xml return Xml.parse( ele.html() );
-	public static function parseNode(_, target:DOMNode):DOMNode return target;
-	public static function parseDOMCollection(value:String, target:DOMNode) {
-		trace( value );
-		trace( target );
-		return target;
-	}*/
 	
 	public static function parseString(name:String, ele:DOMNode, attr:Bool):String return attr?ele.attr(name):ele.text();
 	public static function parseFloat(name:String, ele:DOMNode, attr:Bool):Float return Std.parseFloat( attr?ele.attr(name):ele.text() );
