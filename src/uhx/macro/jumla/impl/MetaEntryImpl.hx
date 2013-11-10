f2e4dde5664a39f4250aa061b8513fed6808807e
Skial Bainn <skial.bainn@gmail.com>
2013-11-10 10:26:58 +0000

    minor

--- src/uhx/macro/jumla/impl/MetaEntryImpl.hx
+++ src/uhx/macro/jumla/impl/MetaEntryImpl.hx
@@ -1,15 +1,29 @@
 package uhx.macro.jumla.impl;
 
+import haxe.macro.Type;
+import haxe.macro.Expr;
+import haxe.macro.Context;
+
 /**
  * ...
  * @author Skial Bainn
  */
-class MetaEntryImpl
-{
+class MetaEntryImpl {
+	
+	public var name(get, set):String;
 
-	public function new() 
-	{
-		
+	public function new(entry:MetadataEntry) {
+		this.entry = entry;
 	}
+	
+	// ++ internal
+	
+	@:noCompletion public var entry:MetadataEntry;
+	
+	private function get_name():String return entry.name;
+	
+	private function set_name(v:String):String return entry.name = v;
+	
+	// -- internal
 	
 }
--- src/uhx/macro/jumla/impl/MetaImpl.hx
+++ src/uhx/macro/jumla/impl/MetaImpl.hx
@@ -1,13 +1,27 @@
 package uhx.macro.jumla.impl;
 
+import haxe.macro.Type;
+import haxe.macro.Expr;
+import haxe.macro.Context;
+import uhu.macro.jumla.Common;
+
 /**
  * ...
  * @author Skial Bainn
  */
 class MetaImpl {
-
-	public function new() {
-		
+	
+	public function new(meta:Metadata) {
+		this.meta = meta;
 	}
+	
+	public function exists(name:String):Bool return Common.exists( meta, name );
+	public function get(name:String):MetaEntryImpl return new MetaEntryImpl( Common.get( meta, name ) );
+	
+	// ++ internal
+	
+	@:noCompletion public var meta:Metadata;
+	
+	// -- internal
 	
 }
