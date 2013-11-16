f851dde66ab5ffd7912f7fdbd0ec86e5e200b5cd
Skial Bainn <skial.bainn@gmail.com>
2013-11-16 11:47:28 +0000

    Moved the tests to uhu-spec repo

--- src/tests/src/example/auto_impl/A.hx
+++
@@ -1,8 +0,0 @@
-package example.auto_impl;
-
-/**
- * ...
- * @author Skial Bainn
- */
-
-typedef A = haxe.macro.MacroType<[example.auto_impl.MyMacro.build()]>;
--- src/tests/src/example/auto_impl/Partial.hx
+++
@@ -1,7 +0,0 @@
-package example.auto_impl;
-
-/**
- * ...
- * @author Skial Bainn
- */
-extern typedef Partial<Const> = haxe.macro.MacroType<''>;
