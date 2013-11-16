857d740a1de40e2b597f6c531ff2b29bd1bd9476
Skial Bainn <skial.bainn@gmail.com>
2013-11-16 11:28:22 +0000

    Moved tem related stuff out and into separate repo.

--- src/uhx/macro/Test.hx
+++ src/uhx/macro/Test.hx
@@ -12,12 +12,14 @@ using uhx.macro.Jumla;
 class Test {
 
 	public static function handler(cls:ClassType, fields:Array<Field>):Array<Field> {
-		//trace( cls.sugar().ancestors.fmeta.get( ':db' ) );
-		trace( cls.sugar().ancestors.filter.by.meta( ':db' ) );
-		//trace( fields.sugar().fmeta.get( ':skip' ) );
-		trace( fields.sugar().filter.by.meta( ':skip' ) );
-		trace( fields.sugar().filter.by.access( AStatic ) );
-		trace( fields.sugar().filter.by.kind( FVar(macro:Array<String>) ) );
+		trace( cls.sugar().ancestors.filter.by.meta( ':db' ).length );
+		trace( fields.sugar().filter.by.meta( ':skip' ).length );
+		trace( fields.sugar().filter.by.access( AStatic ).length );
+		trace( fields.sugar().filter.by.type( macro:String ).length );
+		trace( fields.sugar().filter.by.getter( 'get' ).length );
+		trace( fields.sugar().filter.by.setter( 'never' ).length );
+		trace( fields.sugar().filter.by.name( ~/get_/i ).length );
+		trace( fields.sugar().filter.by.kind( FVar(null, null) ).length );
 		return fields;
 	}
 	
--- src/uhx/macro/jumla/expr/FieldTools.hx
+++ src/uhx/macro/jumla/expr/FieldTools.hx
@@ -51,6 +51,10 @@ class ManyFieldTools {
 
 class FieldCollection {
 	
+	public static function name(v:ManyFields, key:EReg):Array<AField> {
+		return Lambda.filter(v.original, function(f) return key.match( f.name ) ).array();
+	}
+	
 	public static function meta(v:ManyFields, key:String):Array<AField> {
 		return Lambda.filter(v.original, function(f) return f.meta.exists( key ) ).array();
 	}
@@ -60,30 +64,77 @@ class FieldCollection {
 	}
 	
 	public static function kind(v:ManyFields, key:FieldType):Array<AField> {
-		var result = false;
-		
-		Lambda.filter(v.original, function(f) {
-			switch (f.kind) {
-				case FVar(t, e) if(key.getName() == 'FVar'):
-					if (key.getParameters()[0] != null) {
-						
-					}
-					
-				case FProp(g, s, t, e) if(key.getName() == 'FProp'):
-					if (key.getParameters()[2] != null) {
-						
-					}
-					
-				case FFun(m) if(key.getName() == 'FFun'):
-					if (key.getParameters()[0] != null) {
-						
-					}
-					
+		return Lambda.filter(v.original, function(f) {
+			var result = false;
+			
+			switch ([f.kind, key]) {
+				case [FVar(_, _), FVar(_, _)]: result = true;
+				case [FProp(_, _, _, _), FProp(_, _, _, _)]: result = true;
+				case [FFun(_), FFun(_)]: result = true;
 				case _:
 			}
-		} );
-		
-		return result;
+			
+			return result;
+		} ).array();
+	}
+	
+	public static function type(v:ManyFields, key:ComplexType):Array<AField> {
+		return Lambda.filter(v.original, function(f) {
+			var result = false;
+			
+			var ctype = switch (f.kind) {
+				case FVar(ctype, _): ctype;
+				case FProp(_, _, ctype, _): ctype;
+				case FFun(m): m.ret;
+			}
+			
+			if (ctype != null) {
+				
+				switch ([ctype, key]) {
+					case [TPath( { name:n1, pack:p1, params:_ } ), TPath( { name:n2, pack:p2, params:_ } )]:
+						result = p1.concat([n1]).join('.') == p2.concat([n2]).join('.');
+						
+					case [TAnonymous(f1), TAnonymous(f2)]:
+						result = true;
+						for (f in f2) if (!f1.exists( f.name )) {
+							result = false;
+							break;
+						}
+						
+					case _:
+						trace( [ctype, key] );
+				}
+				
+			}
+			
+			return result;
+		} ).array();
+	}
+	
+	public static function getter(v:ManyFields, key:String):Array<AField> {
+		return Lambda.filter(v.original, function(f) {
+			var result = false;
+			
+			switch (f.kind) {
+				case FProp(g, _, _, _): result = g == key;
+				case _:
+			}
+			
+			return result;
+		} ).array();
+	}
+	
+	public static function setter(v:ManyFields, key:String):Array<AField> {
+		return Lambda.filter(v.original, function(f) {
+			var result = false;
+			
+			switch (f.kind) {
+				case FProp(_, s, _, _): result = s == key;
+				case _:
+			}
+			
+			return result;
+		} ).array();
 	}
 	
 }
